46f8f78aa537eb52fb295812cbf9a7251f21ef9e
Lew Kai Ming <kai_1997_ming@hotmail.com>
2020-11-14 15:50:30 +0800

    deploy serverless

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let apiRoutes = require('./api-routes');
 let bodyParser = require('body-parser');
 let express = require('express');
 let mongoose = require('mongoose');
+let serverless = require('serverless-http');
+let cors = require('cors');
+require('dotenv').config();
 
 // Initialize app
 let app = express();
@@ -15,7 +18,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/switches', {
+mongoose.connect('mongodb+srv://Me:' + process.env.PASSWORD + '@cs3219-otot-b.xcwlm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
 
@@ -34,6 +37,7 @@ var port = process.env.PORT || 8080;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello to Switches!'));
 
+app.use(cors());
 app.use('/api', apiRoutes);
 
 // Launch app and listen to specified port
@@ -43,3 +47,4 @@ app.listen(port, function() {
 
 // Export app for testing
 module.exports = app;
+module.exports.handler = serverless(app);
